ce6b08a9d9d06a08dab49095ef66d914870cabda
Theodor Burlacu <76620498+thabur@users.noreply.github.com>
2026-05-09 11:41:06 +0300

    Improve mobile exercise video display

--- mobile-gym/sw.js
+++ mobile-gym/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gym-cockpit-v3";
+const CACHE_NAME = "gym-cockpit-v4";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -15,7 +15,7 @@ const CORE_ASSETS = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
   );
 });
 
@@ -23,7 +23,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
@@ -31,12 +31,12 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
+    fetch(event.request)
+      .then((response) => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
-      });
-    })
+      })
+      .catch(() => caches.match(event.request))
   );
 });
